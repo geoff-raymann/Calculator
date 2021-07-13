d11118351e6b73158d058a2d125923ebfee281c1
geoff-raymann <odiwuorgeoff50@gmail.com>
2021-07-13 17:53:35 +0300

    Add Files to the website

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var add=(number1, number2)=>number1+number2;
+var number1=prompt("Enter a number: ");
+var number2=prompt("Enter another number: ");
+var newNumber1=parseInt(number1);
+var newNumber2=parseInt(number2);
+alert(add(newNumber1, newNumber2));
